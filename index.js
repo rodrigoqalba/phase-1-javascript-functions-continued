9fe65974535a5b9ab2374823315b253fa4194efa
Rodrigo Alba <rodrigoqalba@gmail.com>
2021-10-13 09:48:14 -0400

    commit tree 6bf97362a5258db6625bdce81f608bf2a5f2aac4

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork() {
+    return "function exists"
+}
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective (z = `*`) {
+    return function (i = `special`) {
+        return `You are ${z}${i}${z}!`
+    }
+}
